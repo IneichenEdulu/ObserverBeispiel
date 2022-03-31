d05260806effa0ea48738c43bf8a3dc1edfaca3b
Markus Ineichen <markus.ineichen1@sluz.ch>
2022-03-31 13:45:05 +0200

    Initialisierung

---
+++ ObserverPattern/SportNewsletter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObserverPattern
+{
+    // Konkretes Subjekt
+    public class SportNewsletter : NewsletterSubject
+    {
+        private Newsletter currentNewsletter;
+
+        public SportNewsletter(Newsletter newsletter)
+        {
+            this.currentNewsletter = newsletter;
+        }
+
+        public Newsletter GetNewsletter()
+        {
+            return currentNewsletter;
+        }
+
+        public void SetNewsletter(Newsletter newNewsletter)
+        {
+            currentNewsletter = newNewsletter;
+            Console.WriteLine("Es ist ein neuer Newsletter erschienen");
+            SendNewsletter(currentNewsletter);
+        }
+    }
+}
